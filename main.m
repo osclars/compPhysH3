cd33f77c1c94793d21434b1ecf53411b45c5a621
Oskar <osclars@student.chalmers.se>
2014-12-11 16:10:54 +0100

    worte algorithm in mainfile, not currently working

--- main.m
+++ main.m
@@ -1,30 +1,48 @@
 %% Task 1
 
 tol = 10^-5;
-coarseSize = 11;
+%coarseSize = 11;
 fineSize = 21;
-
-% create source (only copied, fix before use TODO)
-diPoleY = floor(uSize / 2) +1;
-diPoleX1 = diPoleY + floor(d * uSize/2);
-diPoleX2 = diPoleY - floor(d * uSize/2);
-U(diPoleX1,diPoleY) = 1;
-U(diPoleX2,diPoleY) = -1;
-h=L/ (uSize-1);
-source = U/h^2;
-%if ~(j == diPoleY && (i == diPoleX1 || i == diPoleX2))
-
-
+L=1;
+d=0.2;
+source = zeros(fineSize);
+solution = zeros(fineSize);
+residual = zeros(fineSize);
+errorCourse = zeros(fineSize);
+diPoleY = floor(fineSize / 2) +1;
+diPoleX1 = diPoleY + floor(d * fineSize/2);
+diPoleX2 = diPoleY - floor(d * fineSize/2);
+source(diPoleX1,diPoleY) = 1;
+source(diPoleX2,diPoleY) = -1;
 % Presmooth
+for i=1:3
+
+    [solution,errorMain] = gaussSeidel(source,solution);
+    
+end
+
+
 
 % Compute R
 
+residual = source - 4*del2(solution);
+
 % Restrict R to coarser grid
+residualCoarse = restriction(residual);
 
 % Solve del2(E) = R
-
+errorRes = 1;
+while errorRes > tol
+    [errorCourse, errorRes] = gaussSeidel(residualCoarse, errorCourse);
+end
 % Interpolate E to fine grid
-
+errorFine = interpolation(errorCourse);
 % Update phiApprox 
-
+solution = solution + errorFine;
 %Post smooth
+
+for i=1:3
+
+    [solution,errorMain] = gaussSeidel(source,solution);
+    
+end
