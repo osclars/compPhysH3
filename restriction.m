07a1474c4bcb5150e980ad0b486de3ac77fda471
UnniEngedahl <unniengedahl@dhcp-207173.eduroam.chalmers.se>
2014-12-12 19:35:38 +0100

    kommenterat

--- restriction.m
+++ restriction.m
@@ -1,5 +1,8 @@
 function [ redMatrix ] = restriction( finMatrix )
-%RESTRICTION Multigrid method: restricts a finer grid to a coarser one
+%RESTRICTION Multigrid method: restricts a finer grid to a coarser one.
+% All grid points in the coarse grid is weighted by its surounding points
+% in the fin grid according to: it self 1/4, nearest neighbours 1/8, next
+% nearest neighbours 1/16.
 %   finerMatrix = the matrix you would like to reduce
 
 restrictedSize = floor(size(finMatrix)/2)+1; %size of the restricted matrix
@@ -9,21 +12,11 @@ u=finMatrix; %shorter name for the finer grid
 %from fine to coars
 for i=2:restrictedSize-1
     for j=2:restrictedSize-1
-        % for all elements on the diagonal
-        if i==j
-            term1 = 1/4*u(2*i-1,2*j-1);
-            term2 = 1/8*(u(2*i-2,2*j-1)+u(2*i-1,2*j)+u(2*i,2*j-1)+u(2*i-1,2*j-2));
-            term3 = 1/16*(u(2*i-2,2*j-2)+u(2*i-2,2*j)+u(2*i,2*i-2)+u(2*i,2*j));
-            redMartix(i,j)= term1+term2+term3;
-        end
-        %for all off-diagonal elements
         term1 = 1/4*u(2*i-1,2*j-1);
         term2 = 1/8*(u(2*i-2,2*j-1)+u(2*i-1,2*j)+u(2*i,2*j-1)+u(2*i-1,2*j-2));
         term3 = 1/16*(u(2*i-2,2*j-2)+u(2*i-2,2*j)+u(2*i,2*j)+u(2*i,2*j-2));
         redMatrix(i,j)= term1+term2+term3;
-        
     end
 end
-
 end
 
